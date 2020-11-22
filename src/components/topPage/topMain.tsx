a442c727179a46f5fb5d098d84ff91b7fcbf9768
takeyu1013 <yuto1013@gmail.com>
2020-11-22 15:27:27 +0900

    ページ遷移を実装しました

--- src/components/topPage/topMain.tsx
+++ src/components/topPage/topMain.tsx
@@ -1,5 +1,6 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
+import { useHistory } from 'react-router-dom';
 import { IconButton, InputBase } from '@material-ui/core';
 import { Search } from '@material-ui/icons';
 import Paper from '@material-ui/core/Paper';
@@ -18,20 +19,35 @@ const useStyle = makeStyles(() =>
       marginRight: 'auto',
       top: '33%',
       width: '45%'
+    },
+    inputBase: {
+      width: '80%'
     }
   })
 );
 
 const TopMain: FC = () => {
   const classes = useStyle();
+  const [keyword, setKeyword] = useState('');
+  const history = useHistory();
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(event.target.value);
+  };
+  const handleSubmit = () => {
+    history.push('/search/' + keyword);
+  };
   return (
     <div className={classes.background}>
-      <Paper className={classes.paper}>
+      <Paper className={classes.paper} component="form" onSubmit={handleSubmit}>
         <IconButton type="submit">
           <Search />
         </IconButton>
-        <InputBase />
+        <InputBase
+          className={classes.inputBase}
+          placeholder="検索する文字列を入力してください"
+          onChange={handleChange}
+        />
       </Paper>
     </div>
   );
--- src/pages/topPage.tsx
+++ src/pages/topPage.tsx
@@ -1,13 +1,25 @@
 import React, { FC } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import TopHeader from '../components/topPage/topHeader';
 import TopMain from '../components/topPage/topMain';
 
 const TopPage: FC = () => {
   return (
-    <div>
-      <TopHeader />
-      <TopMain />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <TopHeader />
+          <TopMain />
+        </Route>
+        <Route exact path="/search/:keyword">
+          検索結果
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
